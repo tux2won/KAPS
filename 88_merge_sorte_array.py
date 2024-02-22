06aac5c7b38dbcb658c19edcc4236a70c42e3aee
tux2won <114200466+tux2won@users.noreply.github.com>
2024-02-22 21:37:31 +0900

    Leet code_test commit
    테스트용

---
+++ 88_merge_sorte_array.py
@@ -0,0 +1,38 @@
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: None Do not return anything, modify nums1 in-place instead.
+        """
+        nums1 = nums1[:m]
+        nums2 = nums2[:n]
+        nums1 += nums2
+        nums1.sort()
+        return nums1
+
+
+so=Solution()
+so.merge([1,2,3,0,0,0], 3, [2,5,6], 3)
+
+
+
+
+
+                    
+
+        
+
+            
+
+# Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+# Output: [1,2,2,3,5,6]
+# Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
+# The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
+
+# Input: nums1 = [1], m = 1, nums2 = [], n = 0
+# Output: [1]
+# Explanation: The arrays we are merging are [1] and [].
+# The result of the merge is [1].
